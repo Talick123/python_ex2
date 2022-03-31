94a1ebdca0c385fe95a38773688568d051d7bc41
Tali Kalev <talkalev@gmail.com>
2022-03-31 16:54:56 +0300

    run on sql file lines and print the tables name

--- ex2_levyno_talikal.py
+++ ex2_levyno_talikal.py
@@ -1,1 +1,22 @@
-file = open(" ", 'r')
+def convertSQLtoCSV(file_name):
+    file = open(file_name, 'r')
+
+    if not file:
+        print("File cannot be opened")
+
+    for line in file:
+        line = line.split()
+        if line and line[0] == "CREATE" and line[1] == "TABLE":
+            print(line[2] + "\n")
+            # create_new_csv_table(line[2])
+
+
+
+
+# create_new_csv_table(file_name):
+#     csv_file = open(path+name+".csv", "a")
+#     # check insert into
+#     # close file
+
+convertSQLtoCSV(r"C:\Users\Tali\Documents\Hadassah\Second Year\Second Semester\OS2\Python Exercises\demo-sql\demo.sql")
+# convertSQLtoCSV("")
