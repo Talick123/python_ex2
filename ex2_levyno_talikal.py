426895ecb2786b8fe8cb40b8fa043b562f8957a7
nogalevy <nogalevy2@gmail.com>
2022-04-01 14:04:32 +0300

    add insert into

--- ex2_levyno_talikal.py
+++ ex2_levyno_talikal.py
@@ -1,4 +1,3 @@
-
 import os
 
 def convertSQLtoCSV(file_name):
@@ -8,30 +7,47 @@ def convertSQLtoCSV(file_name):
         print("File cannot be opened")
 
     for line in file:
-        line = line.split()
-        if line and line[0] == "CREATE" and line[1] == "TABLE":
-            # print(line[2][1:-1] + "\n")
-            create_new_csv_table(line[2][1:-1], file)
+        line_list = line.split()
+        if line_list and line_list[0] == "CREATE" and line_list[1] == "TABLE":
+            create_new_csv_table(line_list[2][1:-1], file)
+        if line_list and line_list[0] == "INSERT" and line_list[1] == "INTO":
+            insert_into(line[2][1:-1], line)
+    file.close()
 
 
 def create_new_csv_table(file_name, sql_file):
-     
     csv_file = open("./csv_output/" + file_name + ".csv", "w")
+    # csv_file = open("./csv_output/" + file_name + ".csv", "w")
     output_list = []
 
-    for line in sql_file:        
+    for line in sql_file:
         line = line.split()
         if line and line[0][0] == "`":
+            # print("34234 " + type(line))
             output_list.append(line[0][1:-1])
         else:
-            csv_file.write(','.join(output_list) + '\n') 
+            csv_file.write(','.join(output_list) + '\n')
             break
 
     csv_file.close()
-     # insert columns
-
+    return
      # check insert into
-     # close file
+
+
+def insert_into(file_name, line):
+    csv_file = open("./csv_output/" + file_name + ".csv", "a")
+
+    i = line.index('VALUES ')
+    values = line[i + 1 :-1]
+    values = values.split( "),(" )
+
+    csv_file.writelines(values)
+    # print(values)
+    # csv_file.write('\n')
+    csv_file.close()
+    return
+
+
 
 f = os.getcwd().split("\\")
 the_path = "/".join(f)
